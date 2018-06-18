498e598868c60bb2de070ad2d58b61b6e2727d70
sei0o <sei0o@live.jp>
2018-06-18 22:40:32 +0900

    Implemented BSF instruction

--- src/emulator.rs
+++ src/emulator.rs
@@ -37,7 +37,14 @@ impl Emulator {
 
   pub fn do_next_instruction(&mut self) { 
     match self.program_mem[self.pc as usize] {
+      // Byte-oriented file register operations
+      
+      // Bit-oriented file register operations
+      op if (op >> 10) == 0b0101 => instruction::bsf(self),
+      
+      // Literal and control operations
       op if (op >> 11) == 0b101 => instruction::goto(self),
+      
       _ => {
         println!("Not Implemented");
         self.pc += 1;
--- src/instruction.rs
+++ src/instruction.rs
@@ -1,7 +1,28 @@
 use emulator::Emulator;
 
+//
+// Byte-oriented file register operations
+//
+
 pub fn nop(emu: &mut Emulator) {
 }
+
+//
+// Bit-oriented file register operations
+// 
+
+// Bit set f
+pub fn bsf(emu: &mut Emulator) {
+  let instr = emu.program_mem[emu.pc as usize];
+  let b = (instr >> 7) & 0x7;
+  let f = instr & 0x7f;
+  emu.file_reg[f as usize] &= 1 << b;
+  emu.pc += 1;
+}
+
+//
+// Literal and control operations
+//
 
 // Unconditional branch
 pub fn goto(emu: &mut Emulator) {
