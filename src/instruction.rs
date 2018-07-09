0e5f816655f96d22f0b8aa1bfd6e5add432ef36d
sei0o <sei0o@live.jp>
2018-07-09 17:51:34 +0900

    Implemented dumping registers from program

--- src/instruction.rs
+++ src/instruction.rs
@@ -52,6 +52,13 @@ pub fn bsf(emu: &mut Emulator) {
 // Unconditional branch
 pub fn goto(emu: &mut Emulator) {
   let instr = emu.program_mem[emu.pc as usize];
+
+  if instr & 0x7ff == 0x400 {
+    emu.dump_regs();
+    emu.pc += 1;
+    return;
+  }
+
   // TODO: get upper bits from PCLATH(SFR) 
   let pclath_addr = 0;
   emu.pc = (pclath_addr << 11) | (instr & 0x7ff);
